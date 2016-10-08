14ee0a7c8e85804bd96c8424530fdfba0930030c
Dico200 <dico.karssiens@gmail.com>
2016-10-08 21:21:50 +0100

    hi

---
+++ src/main/java/com/redstoner/command/ConfigException.java
@@ -0,0 +1,11 @@
+package com.redstoner.command;
+
+public class ConfigException extends RuntimeException {
+	
+	private static final long serialVersionUID = 2427523120643455606L;
+
+	public ConfigException(String message) {
+		super(message);
+	}
+
+}
